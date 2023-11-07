2f8934d53a414a5b60d05765044880f6a3f48c4d
NiclO1337 <niclas.hugdahl@gmail.com>
2023-11-07 13:21:21 +0100

    Add docstrings to function and define goal function

--- run.py
+++ run.py
@@ -3,17 +3,34 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 def welcome():
+  """
+  Start application on a welcome screen with a message
+  """
   print('\nWelcome message\n')
   print('Press ENTER to continue')
 
 
 def dream():
+  """
+  Screen where user think about their dreams
+  """
   print('Dream')
-  
+
+
+def goal():
+  """
+  Function to ask user to input a specific financial goal
+  """
+  print('Goal')
+
 
 def main():
+  """
+  Main function to call other function in the correct order
+  """
   welcome()
   dream()
+  goal()
 
 
 main()
